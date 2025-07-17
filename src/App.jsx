1cd9fd49d726f0c00c9007f32f3cdb8fb718ae89
Cristian Barrantes <cristiandimas74@gmail.com>
2025-07-16 19:47:50 -0500

    commint inicial

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import  { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import  Loader  from './components/Loader.jsx'
+
+// Componentes lazy (carga perezosa)
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Login = lazy(() => import('./pages/Login.jsx'));
+const Admin = lazy(() => import('./pages/Admin.jsx'));
+const NotFound = lazy(() => import('./pages/NotFound.jsx'));
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* Ruta pública */}
+          <Route path="/login" element={
+            <Suspense fallback={<Loader />}>
+              <Login />
+            </Suspense>
+          } />
+          
+          {/* Ruta protegida - usuarios normales */}
+          <Route path="/" element={
+           
+              <Suspense fallback={<Loader />}>
+                <Home />
+              </Suspense>
+          } />
+          
+          {/* Ruta protegida - solo admin */}
+          <Route path="/admin" element={
+              <Suspense fallback={<Loader />}>
+                <Admin />
+              </Suspense>
+          } />
+          
+          {/* Manejo de errores */}
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
